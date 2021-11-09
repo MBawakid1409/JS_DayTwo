c7ace242d1a9e286cae317182f2899f3a9c28291
MBawakid1409 <m.bawakid.1409@gmail.com>
2021-11-09 15:10:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('Week 1 - Day 2')
+
+console.log('Tuwaiq Academy   JS')
+
+
+/*
+let a = 5;
+let b = a * 2;
+let c = a + b;
+let d = c - a;
+console.log(a, b, c, d);
+*/
+
+//The Homework
+let firstVariable = 46 ;
+let secondVariable = firstVariable * 4;
+let thirdVariable = firstVariable + secondVariable;
+let forthVariable = thirdVariable - secondVariable;
+let firstName = 'Mohammad';
+let familyName = 'Bawakid';
+let myAge = 33;
+let phraseResult = `my name is ${firstName} ${familyName}, my age is ${myAge}`;
+let phraseLength = phraseResult.length;
+let phraseIndex = phraseResult[6];
+
+console.log(firstVariable+ '\n', secondVariable+ '\n', thirdVariable+ '\n', forthVariable+ '\n', firstName, familyName+ '\n', myAge+ '\n', phraseResult+ '\n', phraseLength+ '\n', phraseIndex+ '\n');
